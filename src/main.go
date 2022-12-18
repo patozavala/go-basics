55445a823d409da570ec8021a4bfbf633d33e1a4
patricio <pazavala@uc.cl>
2022-12-18 01:36:12 -0300

    class02: declaration and types of variables

--- src/main.go
+++ src/main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	// constant declaration
+	const pi float64 = 3.14
+	const e = 2.71
+
+	fmt.Println("pi:", pi)
+	fmt.Println("e:", e)
+
+	// declaration of integer variables
+	base := 2
+	var height int = 4
+	var area int
+	area = base * height
+
+	fmt.Println(base, height, area)
+
+	// zero-values
+	var zvInt int
+	var zvFloat float64
+	var zvString string
+	var zvBool bool
+
+	fmt.Println(zvInt, zvFloat, zvString, zvBool)
+
 }
