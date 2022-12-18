c2d1902083ad7047a6c403a579d6afdbce242249
patricio <pazavala@uc.cl>
2022-12-18 01:58:07 -0300

    class03: basic functions

--- src/main.go
+++ src/main.go
@@ -2,28 +2,25 @@ package main
 
 import "fmt"
 
+func rectangleArea(width float64, length float64) float64 {
+	area := width * length
+	return area
+}
+
+func rectanglePerimeter(width float64, length float64) float64 {
+	perimeter := 2 * (width + length)
+	return perimeter
+}
+
+func rectangleData(width float64, length float64) (float64, float64) {
+	area := rectangleArea(width, length)
+	perimeter := rectanglePerimeter(width, length)
+	return area, perimeter
+
+}
+
 func main() {
-	// constant declaration
-	const pi float64 = 3.14
-	const e = 2.71
-
-	fmt.Println("pi:", pi)
-	fmt.Println("e:", e)
-
-	// declaration of integer variables
-	base := 2
-	var height int = 4
-	var area int
-	area = base * height
-
-	fmt.Println(base, height, area)
-
-	// zero-values
-	var zvInt int
-	var zvFloat float64
-	var zvString string
-	var zvBool bool
-
-	fmt.Println(zvInt, zvFloat, zvString, zvBool)
-
+	const width float64 = 5
+	const length float64 = 4
+	fmt.Println(rectangleData(width, length))
 }
